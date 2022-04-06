3db1e7a05a6e2bfb0f7527ae22895c641765fbff
omolayo clement <naijawebmaster1@gmail.com>
2022-04-06 19:11:14 +0100

    initial commit

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom";
 import { Button } from "../partials/Button";
-import { Formik, Field, Form, useFormik } from 'formik';
 import { useNavigate } from "react-router-dom";
-import { Fragment, useState } from 'react'
-import React, { useEffect } from "react";
+import { useState } from 'react'
+import { useEffect } from "react";
 import axios from "axios";
 import swal from 'sweetalert'
 import { useDispatch, useSelector } from "react-redux";
 import setProducts from "../Redux/Actions/ProductsActions";
-import productsReducer from "../Redux/Reducers/ProductsReducers";
+import Carousel from "../components/Carousel"
 
 function Dashboard() {
   const [productPage, setProductsPage] = useState(4)
@@ -37,30 +36,30 @@ function Dashboard() {
   const { Products, User }: any = useSelector((state) => state);
   const state: any = useSelector((state) => state);
   console.log(state);
-  let {data} = Products.Products;
-  if (data == null){
+  let { data } = Products.Products;
+  if (data == null) {
     data = []
   }
   const slicedProduct = data.slice(0, productPage)
   const getUserFromLocalStorage = () => {
     try {
-        return JSON.parse(localStorage.getItem("User")|| '');
+      return JSON.parse(localStorage.getItem("User") || '');
     } catch (error) {
-        return null;
+      return null;
     }
-};
-let UserData = getUserFromLocalStorage()
+  };
+  let UserData = getUserFromLocalStorage()
   return (
     <div>
       <div>
         <section className="bg-blue-900 py-10">
           <div className="md:grid grid-cols-4 gap-0 md:flex-wrap text-left md:text-left allign-left overflow-hidden mx-16">
             <div className="md:col-span-1 mt-10  text-center overflow-hidden">
-          <Link to={"/"}>    <img
+              <Link to={"/"}>    <img
                 className="h-10 w-auto"
                 src="./img/rodufy.svg"
                 alt="Workflow"
-              /> </Link> 
+              /> </Link>
             </div>
             <div className="md:col-span-1 mt-10  hidden md:block  text-center overflow-hidden">
             </div>
@@ -78,7 +77,7 @@ let UserData = getUserFromLocalStorage()
             <div className="md:col-span-1 mt-10 hidden md:block text-center overflow-hidden">
             </div>
             <div className="md:col-span-1 mt-10  text-center overflow-hidden">
-                <Button
+              <Button
                 onClick={
                   () => {
                     localStorage.setItem("User", "");
@@ -86,20 +85,38 @@ let UserData = getUserFromLocalStorage()
                     setTimeout(function () { navigate("/login") }, 1000);
                   }
                 }
-                  loadingText={"Processing"}
-                  // loading={loading} 
-                  text={"Logout"}
-                  btnType={"secondary"}
-                />           </div>
+                loadingText={"Processing"}
+                // loading={loading} 
+                text={"Logout"}
+                btnType={"secondary"}
+              />           </div>
           </div>
         </section>
         <section className="bg-gray-900 py-10">
+          <div className="md:grid grid-cols-1 gap-0 md:flex-wrap text-center md:text-left allign-center overflow-hidden md:mx-16 mx-6">
+            <div className="">
+              <Carousel />
+            </div>
+          </div>
+        </section>
+        <section className="bg-gray-900 py-10">
+          <div className="mx-16">
+          <img
+                className="w-5 inline mr-2"
+                src="./img/up.svg"
+                alt="Workflow"
+              />
+      <h2 className=" inline text-xl leading-8 font-semibold mb-12 text-slate-700">
+      Trending
+            </h2>
+          </div>
           <div className="md:grid grid-cols-2 gap-0 md:flex-wrap text-center md:text-left allign-center overflow-hidden md:mx-16 mx-6">
+            
             {slicedProduct?.map((value: any, index: any) => (
               <div key={index} className="md:col-span-1 overflow-hidden text-white mt-auto mb-auto md:w-3/4 mx-auto cursor-pointer">
                 <div className=" text-center md:text-left allign-center ">
                   <div className=" pt-44 my-auto mt-10 p-20 text-center overflow-hidden h-60 rounded-xl" style={{ backgroundImage: `url(${value.metaImageUrl})` }}>
-                    <h1>
+                    <h1 className="text-elipsis">
                       {value.description}
                     </h1>
                   </div>
@@ -108,18 +125,22 @@ let UserData = getUserFromLocalStorage()
           </div>
 
           <div className="md:w-1/4 md:mx-auto mt-10  text-center overflow-hidden mx-6">
-                <Button
-                onClick={
-                  () => {
-                    setProductsPage(productPage + 4)
-                  }
+            <Button
+              onClick={
+                () => {
+                  setProductsPage(productPage + 4)
                 }
-                  loadingText={"Processing"}
-                  // loading={loading} 
-                  text={"Click to Load More"}
-                  btnType={"secondary"}
-                />           </div>
+              }
+              loadingText={"Processing"}
+              // loading={loading} 
+              text={"Click to Load More"}
+              btnType={"secondary"}
+            />           </div>
         </section>
+
+
+
+
       </div>
     </div>
   )
